1bb15581cefb0ca6b0359263f76edd7e5ab76827
Alive Kuo <alegnadise@gmail.com>
2012-11-05 12:59:51 -0800

    Bug 810780 - [Sound Manager] Hardware Volume Keys should react to phone's current state'

--- apps/system/js/sound_manager.js
+++ apps/system/js/sound_manager.js
@@ -14,31 +14,113 @@
   // This event is generated in shell.js in response to bluetooth headset.
   // Bluetooth headset always assign audio volume to a specific value when
   // pressing its volume-up/volume-down buttons.
+  //
+  // Bluetooth earphone setting is supenior to the phone setting itself.
   window.addEventListener('mozChromeEvent', function(e) {
     var type = e.detail.type;
-    if (type == 'volumeset') {
-      changeVolume(e.detail.value - currentVolume);
+    if (type == 'bluetooth-volumeset') {
+      changeVolume(e.detail.value - currentVolume['bluetooth'], 'bluetooth');
     }
+    // We need to manipulate music type bluetooth earphone after v1
+    //
+    // platform would send mozChromeEvent about which channel is using now
+    // See https://bugzilla.mozilla.org/show_bug.cgi?id=811222
   });
 
-  var currentVolume = 0.5;
+
+  var currentVolume = {
+    'voice': 5,
+    'system': 5,
+    'alarm': 5,
+    'fm': 5,
+    'bt': 5
+  };
   var pendingRequestCount = 0;
 
   // We have three virtual states here:
   // OFF -> VIBRATION -> MUTE
   var muteState = 'OFF';
 
-  SettingsListener.observe('audio.volume.master', 5, function(volume) {
+  SettingsListener.observe('audio.volume.voice_call', 5, function(volume) {
     if (pendingRequestCount)
       return;
 
-    currentVolume = volume * 10;
+    currentVolume['voice'] = volume;
+  });
+  
+  SettingsListener.observe('audio.volume.fm', 5, function(volume) {
+    if (pendingRequestCount)
+      return;
+
+    currentVolume['fm'] = volume;
+  });
+  
+  SettingsListener.observe('audio.volume.alarm', 5, function(volume) {
+    if (pendingRequestCount)
+      return;
+
+    currentVolume['alarm'] = volume;
+  });
+  
+  SettingsListener.observe('audio.volume.bt_sco', 5, function(volume) {
+    if (pendingRequestCount)
+      return;
+
+    currentVolume['bt'] = volume;
   });
 
   var activeTimeout = 0;
-  function changeVolume(delta) {
-    if (currentVolume == 0 ||
-        ((currentVolume + delta) <= 0)) {
+
+  function checkPermission(app) {
+    var mozPerms = navigator.mozPermissionSettings;
+    if (!mozPerms)
+      return false;
+
+    var value = mozPerms.get(permissions[app.manifest.mainStreamType],
+                             app.manifestURL, app.origin, false);
+
+    return (value === 'allow');
+  };
+
+  function onCall() {
+    var telephony = window.navigator.mozTelephony;
+    if (!telephony)
+      return false;
+
+    return telephony.calls.some(function callIterator(call) {
+      return (call.state == 'connected');
+    });
+  };
+
+  function BTEarphoneConnecting() {
+    // Need platform API support for check the bluetooth earphone is connected.
+    var bluetooth = navigator.mozBluetooth;
+    if (!bluetooth)
+      return false;
+
+    return navigator.mozBluetooth.isConnected(0x111E);
+  };
+
+  var currentStream = 'system';
+
+  function getStream(volume) {
+    var app = WindowManager.getCurrentDisplayedApp();
+    if (BTEarphoneConnecting() && onCall()) {
+      return 'bluetooth';
+    } else if (onCall()) { 
+      return 'voice';
+    } else {
+      return 'system';
+    }
+  };
+
+  function changeVolume(delta, stream) {
+    if (!stream) {
+      stream = getStream();
+    }
+
+    if (currentVolume[stream] == 0 ||
+        ((currentVolume[stream] + delta) <= 0)) {
       if (delta < 0) {
         if (muteState == 'OFF') {
           muteState = 'VIBRATION';
@@ -55,8 +137,8 @@
       }
     }
 
-    var volume = currentVolume + delta;
-    currentVolume = volume = Math.max(0, Math.min(10, volume));
+    var volume = currentVolume[stream] + delta;
+    currentVolume[stream] = volume = Math.max(0, Math.min(10, volume));
 
     var overlay = document.getElementById('system-overlay');
     var notification = document.getElementById('volume');
@@ -108,9 +190,38 @@
       return;
 
     pendingRequestCount++;
-    var req = SettingsListener.getSettingsLock().set({
-      'audio.volume.master': currentVolume / 10
-    });
+
+    var req;
+
+    switch (stream) {
+      case 'voice':
+        req = SettingsListener.getSettingsLock().set({
+          'audio.volume.voice_call': currentVolume['voice']
+        });
+        break;
+      case 'bluetooth':
+        req = SettingsListener.getSettingsLock().set({
+          'audio.volume.bt_sco': currentVolume['bluetooth']
+        });
+        break;
+      case 'fm':
+        req = SettingsListener.getSettingsLock().set({
+          'audio.volume.fm': currentVolume['fm']
+        }); 
+        break;
+      case 'alarm':
+        req = SettingsListener.getSettingsLock().set({
+          'audio.volume.alarm': currentVolume['alarm']
+        });
+        break;
+      default:
+        req = SettingsListener.getSettingsLock().set({
+            'audio.volume.system': currentVolume['system']
+        });
+        break;
+    }
+
+    notification.dataset.streamType = stream;
 
     req.onsuccess = function onSuccess() {
       pendingRequestCount--;
